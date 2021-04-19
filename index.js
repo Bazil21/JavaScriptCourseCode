a08409e62f40bd2f8abc72b13ed9399bb94494e7
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-04-19 17:08:17 +0500

    ✔️ Today Work of JavaScript

---
+++ index.js
@@ -0,0 +1,26 @@
+// ALert Function is Used to Popup the Output that written In Dobule Quotation
+alert("Hello World")
+
+//Console.log used for debugging process
+console.log("Hello World")
+
+//Document.write used to write the text inside the Html body
+document.write("Hello World")
+
+// Declaration Of  Variables
+
+// String
+var Name = "Sameem"
+
+//Number
+var age = 20
+var price = 21.55
+
+//Boolean
+var male = true
+
+//Typeof is used to show the type of variable
+console.log(typeof(Name))
+console.log(typeof(age))
+console.log(typeof(price))
+console.log(typeof(male))
